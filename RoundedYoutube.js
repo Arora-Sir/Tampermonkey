12407f8084f9ad450dc5fcf531dc584e985033a7
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2022-11-01 19:41:27 +0530

    Unable to get element in the starting fix

--- RoundedYoutube.js
+++ RoundedYoutube.js
@@ -1,26 +1,33 @@
 // ==UserScript==
 // @name         Rounded Youtube
 // @namespace    https://github.com/Arora-Sir
-// @license      MIT
-// @version      0.3
-// @description  Round the Youtube video watch screen
+// @version      0.32
+// @description  round the Youtube video screen
 // @author       Mohit Arora
-// @match        https://www.youtube.com/*
+// @match        https://www.youtube.com/watch*
 // @icon         https://www.svgrepo.com/show/92784/youtube.svg
 // @updateURL    https://github.com/Arora-Sir/Tampermonkey/blob/main/RoundedYoutube.js
 // @downloadURL  https://github.com/Arora-Sir/Tampermonkey/blob/main/RoundedYoutube.js
 // @grant        none
 // ==/UserScript==
 
-//greasyfork Install Link: https://greasyfork.org/en/scripts/454081-rounded-youtube
-
 (function() {
     'use strict';
     var OuterVideoPlayer = document.querySelector(".html5-video-player");
-    OuterVideoPlayer.style.borderRadius = "15px";
+    
+    if(OuterVideoPlayer!=null){
+        OuterVideoPlayer.style.borderRadius = "15px";
+//             console.log('Not Null');
+    }
+    else{
+        while (OuterVideoPlayer.style.borderRadius != "15px"){
+            OuterVideoPlayer.style.borderRadius = "15px";
+//             console.log('Null');
+        };
+    }
 
-    document.addEventListener("keydown", FKeyPressed, false);
-    function FKeyPressed(f) {
+    document.addEventListener("keydown", checkKeyPressed, false);
+    function checkKeyPressed(f) {
         if(OuterVideoPlayer.style.borderRadius == "0px"){
              OuterVideoPlayer.style.borderRadius = "15px";
         }
@@ -28,11 +35,9 @@
             OuterVideoPlayer.style.borderRadius == "0px"
         }
     }
-    
    // const FullScreenButton = document.getElementsByClassName("ytp-fullscreen-button");
    // FullScreenButton.addEventListener('click', function handleClick() {
    //    console.log('element clicked');
    //         document.querySelector(".html5-video-player").style.borderRadius = "500px";
    // });
-    
 })();
