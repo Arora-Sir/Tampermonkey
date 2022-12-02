5cc69966235ff0362eeda38dd4ad0ae82f621c0d
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2022-12-02 20:56:06 +0530

    Fast implementation of Rounded corners

--- RoundedYoutube.user.js
+++ RoundedYoutube.user.js
@@ -1,38 +1,59 @@
 // ==UserScript==
 // @name         Rounded Youtube
 // @namespace    https://github.com/Arora-Sir
-// @version      0.5
+// @version      1.0
 // @license      MIT
 // @description  Round the Youtube watch video screen
 // @author       Mohit Arora
-// @match        https://www.youtube.com/watch*
+// @match        *://*.youtube.com/*
+// @exclude      *://music.youtube.com/*
+// @exclude      *://*.music.youtube.com/*
 // @icon         https://www.svgrepo.com/show/92784/youtube.svg
 // @updateURL    https://github.com/Arora-Sir/Tampermonkey/raw/main/RoundedYoutube.user.js
 // @downloadURL  https://github.com/Arora-Sir/Tampermonkey/raw/main/RoundedYoutube.user.js
 // @supportURL   https://github.com/Arora-Sir/Tampermonkey/issues
 // @grant        none
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    
-    let OuterVideoPlayer = document.querySelector(".html5-video-player");
-    OuterVideoPlayer.style.borderRadius = "15px";
-
-    window.addEventListener('fullscreenchange', function() {
-        if (document.fullscreenElement) {
+function setYTRoundCorners (evt) {
+  let checkForRoundYT_TimerCheck
+  function checkForRoundYT () {
+    try {
+      let OuterVideoPlayer =
+        document.querySelector('.html5-video-player') ??
+        document.querySelector('#container.ytd-player > div')
+      OuterVideoPlayer.style.borderRadius = '15px' //Initial State
+      document.addEventListener(
+        'fullscreenchange',
+        function () {
+          if (document.fullscreenElement) {
             //console.log(`Element: ${document.fullscreenElement.id} entered fullscreen mode.`);
-            OuterVideoPlayer.style.borderRadius = "0px";
-        } else {
+            OuterVideoPlayer.style.borderRadius = '0px'
+          } else {
             //console.log('Leaving fullscreen mode.');
-            OuterVideoPlayer.style.borderRadius = "15px";
-        }
-    }, true);
+            OuterVideoPlayer.style.borderRadius = '15px'
+          }
+        },
+        true
+      )
+    } catch (error) {
+      //console.log("[RoundedYoutube]: " + error.message);
+      return
+    }
+    clearInterval(checkForRoundYT_TimerCheck)
+  }
+  checkForRoundYT_TimerCheck = setInterval(checkForRoundYT, 100)
+}
 
+;(function () {
+  'use strict'
+  setYTRoundCorners()
+  //RoundYT
+  //window.addEventListener("yt-navigate-finish", RoundYoutube, true);
 
-    //Old Method Limitations: (Double click listner not added, not work sometimes!)
-    /*
+  //Old Method Limitations: (Double click listner not added, not work sometimes!)
+  /*
     let OuterVideoPlayer = document.querySelector(".html5-video-player");
     let FullScreenButton = document.querySelector('[title="Full screen (f)"]');
 
@@ -61,5 +82,4 @@
         }
     }
 */
-
-})();
+})()
